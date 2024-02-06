79199aad615fb96da898374bb280a8b1eacdce78
c3657891 <isaacafrifa27@gmail.com>
2024-02-06 11:22:04 +0000

    Week 1 done

---
+++ WeeklyExercises/1GettingStarted/Isaac.java
@@ -0,0 +1,8 @@
+public class Isaac {
+    public static void main(String[] args) {
+        int a = 10;
+        int b =20;
+        int sum = a+b;
+        System.out.println("sum of two numbers is "+sum);
+    }
+}
---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,13 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        // Personal details
+        String name = "Isaac";
+        String address = "111 Headingley Road";
+        String Phonenumber = "004456737849364983675498367";
+        int age = 26;
+        System.out.println("Hi I am "+name);
+        System.out.println("I just turned "+age);
+        System.out.println("I live at "+address);
+        System.out.println("You can call me on "+Phonenumber);
+    }
+}
